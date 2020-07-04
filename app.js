729c01e640eae1e50845aeebc4f27586062f7c88
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2020-07-04 12:38:57 +0100

    Setup template engine

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, 'index.css')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Library' });
+});
 
-app.listen(3000, function(){
-    debug(`Express app listening on port ${chalk.green('3000')}`);
-})
+app.listen(port, () => {
+  debug(`Express app listening on port ${chalk.green(port)}`);
+});
